abf34a7c7976b7210706358517e3ff482ea32d56
koalavc <vincec362@gmail.com>
2015-09-11 14:10:00 -0400

    Fixed changes

--- hello.cpp
+++ hello.cpp
@@ -1,23 +1,9 @@
 #include <iostream>
 using namespace std;
 
-<<<<<<< HEAD
 int main() {
 	string username;
 	string yourname;
-=======
-int main () {
-  string username;
-  string yourname;
-
-  username = "Alice";
-
-  cout << "Please enter your name!\n\n";
-
-  cin >> yourname;
-
-  cout << "Hello\n\n" << yourname << ", my name is << username "!"\n\n;
->>>>>>> origin/master
 
 	username = "Vincent";
 
